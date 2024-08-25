ce1ce79514b7ca862f904ea4b052c35b5e0fccc8
js-kayllon <kayllon.rosa@escola.pr.gov.br>
2024-08-24 22:15:42 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,1 @@
+function setup() { createCanvas(400, 400); background("lightgrey") textFont("Comic Sans MS"); textSize(30) textAlign(CENTER,CENTER); } function draw(){ text("Aprendendo JS no Alura",200,200) }
